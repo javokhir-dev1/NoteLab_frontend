3f20ca51b83b1e81cb9ca2406cd89b65a3c15d0f
javokhir-dev1 <javokhir.dev1@gmail.com>
2025-08-12 17:06:49 -0700

    Home page ozgartirildi folderlar qoshildi

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -6,12 +6,16 @@ const apiUrl = import.meta.env.VITE_API_URL;
 import "./css/home.css"
 
 function Home() {
+    const token = JSON.parse(localStorage.getItem("token"))
     const [user, setUser] = useState({ email: "", username: "" })
+    const [folders, setFolders] = useState([])
+
     const username = user.username
         ? user.username.charAt(0).toUpperCase() + user.username.slice(1).toLowerCase()
         : "";
+
     const navigate = useNavigate()
-    const token = JSON.parse(localStorage.getItem("token"))
+
     useEffect(() => {
         if (!token || !token.token || !token.success) {
             localStorage.removeItem("token");
@@ -47,14 +51,42 @@ function Home() {
             }
 
             fetchData()
+
+            async function foldersData() {
+                try {
+                    const res = await fetch(`${apiUrl}/folders/show-folder`, {
+                        method: "GET",
+                        headers: {
+                            Authorization: `Bearer ${token.token}`
+                        }
+                    })
+
+                    const json = await res.json()
+                    setFolders(json.data)
+                } catch (err) {
+                    console.log(err)
+                }
+            }
+
+            foldersData()
         }
-    }, [token, navigate])
+    }, [navigate])
 
 
     return (
         <div className="home_page">
             <div className="home_left">
+
                 <h3 className="user_name">{username}</h3>
+                <div className="create_folder_btn_box">
+                    <button className="create_folder_btn">
+                        <i class='bxr  bx-folder-plus'  ></i> Create Folder
+                    </button>
+                </div>
+
+                <div className="folders_box">
+                    {folders.map((folder) => <div key={folder._id} className="folder_item">{folder.noteName}</div>)}
+                </div>
             </div>
             <div className="home_right"></div>
         </div>
